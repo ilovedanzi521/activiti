390299a5b0826307dd822fcf3bab80efbcf6011e
wanglei <wanglei@ysstech.com>
2019-06-25 09:49:43 +0800

    [modify-问题修复] 问题原因:修改启动方法 解决方法: 影响范围: BugID   :

--- src/main/java/com/win/dfas/ActivitiApplication.java
+++ src/main/java/com/win/dfas/ActivitiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
 /**
