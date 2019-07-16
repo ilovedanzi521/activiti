6c11651287cc79648858a1df4846ecd745c730f2
wanglei <wanglei@ysstech.com>
2019-07-16 15:21:45 +0800

    [modify-问题修复] 问题原因:modify 解决方法: 影响范围: BugID   :

---
+++ src/main/java/com/win/dfas/bpm/dto/QueryAndStartFlowReqDTO.java
@@ -0,0 +1,28 @@
+package com.win.dfas.bpm.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * 包名称：com.win.dfas.bpm.dto
+ * 类名称：QueryAndStartFlowReqDTO
+ * 类描述：查询并启动流程接口合并----hechengheng要求更改、是否违背单一原则？
+ * 创建人：@author wanglei
+ * 创建时间：2019/7/16/13:49
+ */
+@Data
+public class QueryAndStartFlowReqDTO {
+    private String productCode;
+    private String investConstitute;
+    private String transactionDirection;
+    private String userId; //流程用户
+    private String groupId; //流程用户组
+    //    private String
+    private String taskType;
+    private BigDecimal amt;
+    private String processId;
+    private String processDefId;
+    private String taskId;
+    private String permit="0"; //0-通过，1-是不通过
+}
---
+++ src/main/java/com/win/dfas/constant/BpmExceptionEnum.java
@@ -0,0 +1,50 @@
+/****************************************************
+ * 创建人：     @author wanglei
+ * 创建时间:    2019年6月24日/上午9:51:58
+ * 项目名称：  dfas-common-bpm
+ * 文件名称: BpmExceptionEnum.java
+ * 文件描述: @Description: TODO(用一句话描述该文件做什么)
+ *
+ * All rights Reserved, Designed By 投资交易团队
+ * @Copyright:2016-2019
+ *
+ ********************************************************/
+
+package com.win.dfas.constant;
+
+import com.win.dfas.common.exception.IExceptionEnum;
+
+/**
+ * 包名称： com.win.dfas.constant
+ * 类名称：BpmExceptionEnum
+ * 类描述：流程异常枚举
+ * 创建人：@author wanglei
+ * 创建时间：2019年6月24日/上午9:51:58
+ *
+ */
+public enum BpmExceptionEnum implements IExceptionEnum {
+
+    // 认证异常枚举
+    SYSTEM_ERR("800000", "系统错误"),
+//    ID_NOT_NULL("100000", "ID不能为空")
+    ;
+
+    private String code;
+    private String msg;
+
+    BpmExceptionEnum(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMsg() {
+        return msg;
+    }
+
+}
