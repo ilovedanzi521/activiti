b5fb6239513664fcf133c8e3af0e1f061aeaabd9
wanglei <wanglei@ysstech.com>
2019-08-27 11:34:50 +0800

    [modify-问题修复] 问题原因:前端升级 解决方法: 影响范围: BugID   :

---
+++ src/main/frontend/src/components/fDialog.ts
@@ -0,0 +1,9 @@
+/* @win_fdialog组件
+ * @Author: mikey.zhaopeng
+ * @Date: 2019-06-05 14:01:33
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2019-08-24 09:53:35
+ */
+import "./style/fdialog.scss";
+import { Dialog } from "element-ui";
+export const WinFdialog = Dialog;
